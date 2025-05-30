5211d6eeac8711983e26b3e71f8b01917f3ed66e
Pedro <pedrosilvadcarmosouza@gmail.com>
2025-05-29 21:06:08 -0300

    Botoes

--- models/Produto.js
+++ models/Produto.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
 const produtoSchema = new mongoose.Schema({
-  nome: String,
-  preco: Number,
-  quantidade: Number,
-  tamanho: String,
-  cor: String
+  nome: { type: String, required: true },
+  preco: { type: Number, required: true },
+  quantidade: { type: Number, required: true },
+  tamanho: { type: String, required: true },
+  cor: { type: String, required: false }
+});
+
+// Adiciona um campo virtual "id" para substituir "_id" no front-end
+produtoSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+  }
 });
 
 module.exports = mongoose.model('Produto', produtoSchema);
